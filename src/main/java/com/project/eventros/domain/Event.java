87c27ce0d7752188adcdcbbb3a4ac21fec69e53c
nitinsomani2405 <nitin12abhi@gmail.com>
2025-07-20 18:11:00 +0530

    added entities

--- src/main/java/com/project/eventros/domain/Event.java
+++ src/main/java/com/project/eventros/domain/Event.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -59,6 +60,9 @@ public class Event {
     @ManyToMany(mappedBy = "staffingEvents")
     private List<User>staff=new ArrayList<>();
 
+    @OneToMany(mappedBy = "event",cascade = CascadeType.ALL)
+    private List<TicketType>ticketTypes=new ArrayList<>();
+
     @CreatedDate
     @Column(name = "created_at",nullable=false,updatable=false)
     private LocalDateTime createdAt;
@@ -66,4 +70,17 @@ public class Event {
     @LastModifiedDate
     @Column(name="updated_at",nullable=false)
     private LocalDateTime updatedAt;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(id, event.id) && Objects.equals(name, event.name) && Objects.equals(startTime, event.startTime) && Objects.equals(endTime, event.endTime) && Objects.equals(venue, event.venue) && Objects.equals(salesStart, event.salesStart) && Objects.equals(salesEnd, event.salesEnd) && status == event.status && Objects.equals(createdAt, event.createdAt) && Objects.equals(updatedAt, event.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, startTime, endTime, venue, salesStart, salesEnd, status, createdAt, updatedAt);
+    }
 }
